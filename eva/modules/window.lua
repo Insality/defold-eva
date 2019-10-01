525ad209d6a87b7e138b060d1195c93903e47c8d
Insality <insality@gmail.com>
2019-10-02 00:42:17 +0300

    window module update

--- eva/modules/window.lua
+++ eva/modules/window.lua
@@ -11,39 +11,70 @@ local logger = log.get_logger("eva.window")
 local M = {}
 
 
+local function get_settings(window_id)
+	local data = M._eva.app.window_settings
+	assert(data, "Window settings is not setup")
+
+	local settings = {}
+	luax.table.extend(settings, data.default)
+	luax.table.extend(settings, data[window_id])
+	return settings
+end
+
+
 --- Load the game scene
 -- @function eva.window.show_scene
 function M.show_scene(scene_id, data)
-	monarch.show(scene_id, nil, data, function()
-		local window_data = M._eva.app.window
-		window_data.last_scene = scene_id
-		M._eva.events.screen(window_data.last_scene, window_data.last_window)
+	logger:debug("Show scene", { scene_id = scene_id })
+	local settings = get_settings(scene_id)
+
+	-- Hello, Pyramids!
+	settings.before_show_scene(function()
+		monarch.show(scene_id, nil, data, function()
+			settings.after_show_scene()
+
+			local window_data = M._eva.app.window
+			window_data.last_scene = scene_id
+			M._eva.events.screen(window_data.last_scene, window_data.last_window)
+		end)
 	end)
 end
 
 
 --- Show the game window
+-- It will close current window and open new, if any opened
+-- It can be popup on popup, so don't close prev. window
+-- Can add window in queue to open it after current one is closed
+-- Window callbacks will be separated module? Calling with ID
 -- @function eva.window.show
-function M.show(window_id, data)
-	monarch.show(window_id, nil, data)
+function M.show(window_id, data, in_queue)
+	logger:debug("Show window", { window_id = window_id })
+	local settings = get_settings(window_id)
 
-	local window_data = M._eva.app.window
-	window_data.last_window = window_id
-	M._eva.events.screen(window_data.last_scene, window_data.last_window)
+	-- Hello, Pyramids again!
+	settings.before_show_window(function()
+		monarch.show(window_id, nil, data, function()
+			settings.after_show_window()
+
+			local window_data = M._eva.app.window
+			window_data.last_window = window_id
+			M._eva.events.screen(window_data.last_scene, window_data.last_window)
+		end)
+	end)
 end
 
 
 --- Check is window is opened now
 -- @function eva.window.is_opened
 function M.is_opened(window_id)
-
+	return monarch.top() == hash(window_id)
 end
 
 
 --- Close window by id or last window
 -- @function eva.window.close
 function M.close(window_id)
-
+	-- window_id = window_id or M._eva.app.last_window
 end
 
 
@@ -54,18 +85,13 @@ function M.close_all()
 end
 
 
-local function get_settings(window_name)
-	local data = M._eva.app.window_settings
-	assert(data, "Window settings is not setup")
-
-	local settings = {}
-	luax.table.extend(settings, data.default)
-	luax.table.extend(settings, data[window_name])
-	return settings
-end
+-- TODO
+-- Keep context to return from window to window
+-- Make window queue, to open one window after already opened
 
 
 --- Appear functions for all windows
+-- Need to call inside window
 -- @function eva.window.appear
 function M.appear(window_name, cb)
 	local data = M._eva.app.window
@@ -89,6 +115,7 @@ end
 
 
 --- Disappear functions for all windows
+-- Need to call inside window
 -- @function eva.window.disappear
 function M.disappear(window_name, cb)
 	local data = M._eva.app.window
@@ -105,6 +132,10 @@ function M.disappear(window_name, cb)
 			data.transitions = data.transitions - 1
 			-- TODO: If it was popup on popup??
 			M._eva.app.last_window = nil
+
+			if cb then
+				cb()
+			end
 		end)
 	end)
 end
@@ -120,7 +151,7 @@ end
 function M.before_game_start()
 	M._eva.app.window = {
 		last_window = nil,
-		last_scene = "loader",
+		last_scene = "loader", -- TODO: To const or auto-calc?
 		transitions = 0,
 	}
 end
--- main/window_settings.lua
+++ main/window_settings.lua
@@ -4,6 +4,10 @@
 -- 	- render_order - number 0..15 to gui.set_render_order
 -- 	- appear_func - function(settings, cb) on window show
 -- 	- disappear_func - function(settings, cb) on window close
+-- 	- before_show_scene - function before show scene
+-- 	- after_show_scene - function after show scene
+-- 	- before_show_window - function before show window
+-- 	- after_show_window - function after show window
 -- callback should be executed always!
 
 
@@ -20,11 +24,39 @@ local function disappear_simple(settings, cb)
 end
 
 
+local function before_show_scene(settings, cb)
+	cb()
+end
+
+
+local function after_show_scene(settings, cb)
+	if cb then
+		cb()
+	end
+end
+
+
+local function before_show_window(settings, cb)
+	cb()
+end
+
+
+local function after_show_window(settings, cb)
+	if cb then
+		cb()
+	end
+end
+
+
 local M = {
 	["default"] = {
 		render_order = 10,
 		appear_func = appear_simple,
 		disappear_func = disappear_simple,
+		before_show_scene = before_show_scene,
+		after_show_scene = after_show_scene,
+		before_show_window = before_show_window,
+		after_show_window = after_show_window,
 
 		test_value = "test",
 	},
