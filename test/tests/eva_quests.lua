4f0f8790966b2aab04b2e9968a621288d83c287a
Insality <insality@gmail.com>
2019-10-12 02:50:40 +0300

    Add eva quests tests placeholders

--- test/tests/eva_quests.lua
+++ test/tests/eva_quests.lua
@@ -13,5 +13,41 @@ return function()
 			assert(luax.table.contains(current, "quest_4"))
 			assert(#current == 2)
 		end)
+
+		it("Should catch events", function()
+
+		end)
+
+		it("Should end quests after complete tasks", function()
+
+		end)
+
+		it("Should start new quests after complete other", function()
+
+		end)
+
+		it("Should check start quests after changing tokens", function()
+
+		end)
+
+		it("Should not complete, before become available (offline quests)", function()
+
+		end)
+
+		it("Should can be required by multiply quests", function()
+
+		end)
+
+		it("Should send progress events", function()
+
+		end)
+
+		it("Should send task complete events", function()
+
+		end)
+
+		it("Should correct save quest progress", function()
+
+		end)
 	end)
 end
