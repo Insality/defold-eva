2e860285ee5bb5e41666afda17806825eabde64d
Insality <insality@gmail.com>
2019-10-20 13:30:32 +0300

    Update tiled resources

--- hextest/tiled/maps/resources.tsx
+++ hextest/tiled/maps/resources.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7aa2b2c6fb704a289833a475e376cc607afed861fddf195d138be01bb2b90c52
+oid sha256:31d8b845dd9f0a4fa00d1ed4a80a5174497dcad3d42e5c8f4721b127a0244d32
 size 12198
