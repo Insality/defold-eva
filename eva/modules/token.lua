acf145fe01d487b57070a16bba3eb2b0f39d1c0d
Insality <insality@gmail.com>
2019-10-30 22:00:58 +0300

    Separate tokens to tokens and token module (stub)

---
+++ eva/modules/token.lua
@@ -0,0 +1,11 @@
+--- Eva token module
+-- Provide API to add single token to profile
+-- Tokens is basic item, like money, energy or items.
+-- For adding group of tokens use eva.tokens
+-- @submodule eva
+
+
+local M = {}
+
+
+return M
