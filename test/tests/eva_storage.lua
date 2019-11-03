53958121620e8a0a8207f01f2067254911f35d11
Insality <insality@gmail.com>
2019-11-03 16:21:43 +0300

    Add eva_storage tests

--- test/tests/eva_storage.lua
+++ test/tests/eva_storage.lua
@@ -6,7 +6,34 @@ return function()
 			eva.init("/resources/tests/eva_tests.json")
 		end)
 
-		it("Storage test", function()
+		after(function()
+			eva.saver.delete()
+		end)
+
+		it("Should correct add/get storage value", function()
+			eva.storage.set("string_value", "value")
+			eva.storage.set("number_value", 20)
+			eva.storage.set("bool_value", true)
+
+			assert(eva.storage.get("string_value") == "value")
+			assert(eva.storage.get("number_value") == 20)
+			assert(eva.storage.get("bool_value") == true)
+
+			eva.storage.set("number_value", 30)
+			assert(eva.storage.get("number_value") == 30)
+		end)
+
+		it("Should correct save values after save/load", function()
+			eva.storage.set("string_value", "value")
+			eva.storage.set("number_value", 20)
+			eva.storage.set("bool_value", true)
+
+			eva.saver.save()
+			eva.init("/resources/tests/eva_tests.json")
+
+			assert(eva.storage.get("string_value") == "value")
+			assert(eva.storage.get("number_value") == 20)
+			assert(eva.storage.get("bool_value") == true)
 		end)
 	end)
 end
