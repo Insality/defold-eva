7293bb212434cc9271b0514ed8206045273ce75e
Insality <insality@gmail.com>
2019-11-06 17:37:16 +0300

    Add quest_settings stub

--- eva/resources/quests_settings_example.lua
+++ eva/resources/quests_settings_example.lua
@@ -1,4 +1,39 @@
 local M = {}
 
 
+function M.is_can_start(quest_id, quest)
+	return true
+end
+
+
+function M.is_can_complete(quest_id, quest)
+	return true
+end
+
+
+function M.is_can_event(quest_id, quest)
+	return true
+end
+
+
+function M.on_quest_start(quest_id, quest)
+
+end
+
+
+function M.on_quest_progress(quest_id, quest)
+
+end
+
+
+function M.on_quest_task_completed(quest_id, quest)
+
+end
+
+
+function M.on_quest_completed(quest_id, quest)
+
+end
+
+
 return M
