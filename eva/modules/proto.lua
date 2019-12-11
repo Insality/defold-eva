2d44f11d659a436089538bf13486392bfb7ad10d
Insality <insality@gmail.com>
2019-12-12 00:27:13 +0300

    Polish default values in proto decode

--- eva/modules/proto.lua
+++ eva/modules/proto.lua
@@ -12,7 +12,47 @@ local logger = log.get_logger("eva.proto")
 local M = {}
 
 local OPTIONAL = "optional"
+local REPEATED = "repeated"
 local MESSAGE = "message"
+local VALUE = "value"
+local MAP = "map"
+
+
+local function update_with_default_messages(proto_type, data)
+	for name in pb.fields(proto_type) do
+		local _, _, type, _, label = pb.field(proto_type, name)
+		local _, _, field_type = pb.type(type)
+
+		if field_type == MESSAGE then
+			-- Nest messages
+			if label == OPTIONAL then
+				if not data[name] then
+					data[name] = M.get(type)
+				else
+					data[name] = M.decode(type, M.encode(type, data[name]))
+				end
+			end
+
+			-- Repeated list
+			if label == REPEATED then
+				for k, v in pairs(data[name]) do
+					data[name][k] = M.decode(type, M.encode(type, v))
+				end
+			end
+		end
+
+		-- Repeated map (key-value)
+		-- In repeated map we should get value inside Entry
+		if field_type == MAP then
+			if label == REPEATED then
+				for k, v in pairs(data[name]) do
+					local _, _, repeated_type = pb.field(type, VALUE)
+					data[name][k] = M.decode(repeated_type, M.encode(repeated_type, v))
+				end
+			end
+		end
+	end
+end
 
 --- Get empty template from proto type
 -- @function eva.proto.get
@@ -33,15 +73,13 @@ end
 --- Decode protobuf
 -- @function eva.proto.decode
 function M.decode(proto_type, bytes)
-	local data =  pb.decode(proto_type, bytes)
+	local settings = app.settings.proto
+	local data = pb.decode(proto_type, bytes)
 
 	-- Fill nested messages with default values
-	for name in pb.fields(proto_type) do
-		local _, _, type, _, label = pb.field(proto_type, name)
-		local _, _, field_type = pb.type(type)
-		if not data[name] and label == OPTIONAL and field_type == MESSAGE then
-			data[name] = M.get(type)
-		end
+	-- Add default fields even in repeated and map fields
+	if settings.default_nest_messages then
+		update_with_default_messages(proto_type, data)
 	end
 
 	return data
