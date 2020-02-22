354ea307ccd59ae7eab99de5997fd1b37f0b2ed4
Insality <insality@gmail.com>
2020-02-22 03:51:59 +0300

    Correct update lfs file status

--- hextest/tiled/tilesets/ingame.tsx
+++ hextest/tiled/tilesets/ingame.tsx
@@ -1,18 +1,3 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.5" name="ingame" tilewidth="55" tileheight="57" tilecount="1" columns="0">
- <grid orientation="orthogonal" width="1" height="1"/>
- <tile id="0">
-  <properties>
-   <property name="__object_name" value="cursor"/>
-   <property name="__go_path" value="/hextest/assets/ingame/cursor/cursor.go"/>
-   <property name="__image_name" value="cursor"/>
-  </properties>
-  <image width="55" height="57" source="../images/ingame/cursor.png"/>
-  <objectgroup draworder="index">
-   <object id="1" x="27.5" y="28.5">
-    <point/>
-   </object>
-  </objectgroup>
- </tile>
-
-</tileset>
+version https://git-lfs.github.com/spec/v1
+oid sha256:174aef17bfb5857ac2a9ce20d4d6bb0e4e0d73361d9d1a7d2ff81da1d7512b8d
+size 643
