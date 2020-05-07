71a48ceb684e9218b647bcc8b63cdff0da00a74c
Insality <insality@gmail.com>
2020-05-07 20:09:39 +0300

    Add insality_games intro scene

---
+++ eva/assets/insality_games/insality_games.go
@@ -0,0 +1,30 @@
+components {
+  id: "insality_games"
+  component: "/eva/assets/insality_games/insality_games.gui"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+components {
+  id: "insality_games1"
+  component: "/eva/assets/insality_games/insality_games.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
