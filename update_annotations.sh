adfcfe180dc734012282abc473b7def725e69011
Insality <insality@gmail.com>
2020-10-12 01:01:46 +0300

    Update annotations (one file)

---
+++ update_annotations.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+emmylua_generator_path=~/code/lua/emmylua-from-ldoc-annotations
+
+
+echo "Update EmmyLua annotations from ldoc"
+original_path=$(pwd)
+bash $emmylua_generator_path/export.sh $original_path
+mv $emmylua_generator_path/annotations.lua $original_path/annotations.lua
+
+
+echo "Update annotations from protofiles"
+
+~/code/lua/emmylua-protoc-annotations/export.sh \
+	~/code/defold/defold-eva/eva/resources \
+	~/code/defold/defold-eva/eva/resources/eva.proto \
+	>> $original_path/annotations.lua
+
+
