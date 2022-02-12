5cec5bfa641bc5f16dff21cc09a73d592cce12a2
Insality <insality@gmail.com>
2022-02-12 13:22:50 +0200

    Update the eva.server module. Add nakama adapter and all server connect/reconnect handlers

--- eva/modules/server.lua
+++ eva/modules/server.lua
@@ -5,33 +5,149 @@
 
 local app = require("eva.app")
 local log = require("eva.log")
-local server_helper = require("eva.modules.server.server_helper")
+local luax = require("eva.luax")
+local const = require("eva.const")
+local Event = require("eva.event")
+
+local game = require("eva.modules.game")
+local proto = require("eva.modules.proto")
+local saver = require("eva.modules.saver")
+
+local server_nakama_helper = require("eva.modules.server.server_nakama_helper")
 
 local logger = log.get_logger("eva.server")
+
 
 local M = {}
 
 
---- Login at playfab server
--- @function eva.server.login
+--- Reset utilities data on connect to the server
+-- @local
+local function on_connected()
+	app.server_data.last_connection_time = game.get_time()
+	app.server_data.connection_attempt = 0
+end
+
+
+--- Schedule the next reconnect server timer
+-- @local
+local function schedule_reconnect()
+	app.server_data.connection_attempt = app.server_data.connection_attempt + 1
+	local reconnect_time = app.settings.server.reconnect_attempts_time
+
+	local index = luax.math.clamp(app.server_data.connection_attempt, 1, #reconnect_time)
+	local time = reconnect_time[index]
+
+	if app.server_data.reconnect_timer_id then
+		timer.cancel(app.server_data.reconnect_timer_id)
+	end
+
+	app.server_data.reconnect_timer_id = timer.delay(time, false, function()
+		app.server_data.reconnect_timer_id = nil
+		server_nakama_helper.connect(app.server_data.nakama_config, app.server_data.nakama_socket, app[const.EVA.SERVER].token)
+	end)
+
+	logger:info("Schedule reconnect", { time = time, attempt = app.server_data.connection_attempt })
+end
+
+
+--- The daemon to check server sessions and reconnections
+-- @local
+local function connection_daemon()
+	if app.server_data.is_connected or app.server_data.reconnect_timer_id or app.server_data.is_connecting then
+		server_nakama_helper.refresh_session()
+		return
+	end
+	server_nakama_helper.connect(app.server_data.nakama_config, app.server_data.nakama_socket, app[const.EVA.SERVER].token)
+end
+
+
+--- Login at nakama server
+-- @function eva.server.connect
 -- @tparam function callback Callback after login
-function M.login(callback)
+function M.connect(callback)
 	local settings = app.settings.server
-	if not settings.is_enabled then
+	if settings.is_disabled then
 		logger:debug("The server is disabled")
 		return
 	end
 
-	logger:debug("Start connection to PlayFab server")
-	server_helper.setup_playfab()
+	local host = settings.server_host
+	local port = settings.server_port
+	local ssl = settings.use_ssl
+	logger:debug("Start connection to Nakama server", { host = host, port = port, ssl = ssl })
+
+	server_nakama_helper.connect(app.server_data.nakama_config, app.server_data.nakama_socket, app[const.EVA.SERVER].token, function()
+		on_connected()
+		if callback then
+			callback(app.server_data.nakama_session)
+		end
+	end)
 end
 
 
---- Send save to the server
--- @function eva.server.send_save
--- @tparam string json_data JSON data
-function M.send_save(json_data)
+--- Return is currently server connected
+-- @function eva.server.is_connected
+-- @treturn boolean If server connected
+function M.is_connected()
+	return app.server_data.is_connected
+end
 
+
+--- Return nakama client
+-- @function eva.server.get_client
+-- @treturn table
+function M.get_client()
+	return app.server_data.nakama_config
+end
+
+
+--- Return nakama socket
+-- @function eva.server.get_socket
+-- @treturn table
+function M.get_socket()
+	return app.server_data.nakama_socket
+end
+
+
+function M.before_eva_init()
+	local settings = app.settings.server
+	app.server_data = {
+		nakama_config = server_nakama_helper.create_nakama_client(settings.server_host, settings.server_port, settings.use_ssl),
+		nakama_socket = nil,
+		nakama_session = nil,
+		last_connection_time = nil,
+		connection_attempt = 0,
+		reconnect_timer_id = nil,
+		daemon_timer_id = nil,
+		is_connected = false,
+		is_connecting = false,
+	}
+	app.server_events = {
+		event_disconnect = Event(),
+		event_error = Event(),
+		event_notification = Event(),
+		event_matchdata = Event(),
+		event_matchpresence = Event(),
+		event_matchmakermatched = Event(),
+		event_statuspresence = Event(),
+		connect_status = Event(),
+	}
+	app.server_data.nakama_socket = server_nakama_helper.create_nakama_socket(app.server_data.nakama_config)
+
+	server_nakama_helper.before_eva_init(app.server_data.nakama_config, app.server_data.nakama_socket)
+end
+
+
+function M.on_eva_init()
+	app[const.EVA.SERVER] = proto.get(const.EVA.SERVER)
+	saver.add_save_part(const.EVA.SERVER, app[const.EVA.SERVER])
+
+	app.server_events.event_disconnect:subscribe(function()
+		schedule_reconnect()
+	end)
+
+	app.server_data.daemon_timer_id = timer.delay(app.settings.server.check_connection_timer, true, connection_daemon)
 end
 
 
---
+++ eva/modules/server/server_nakama_helper.lua
@@ -0,0 +1,243 @@
+-- Adapter for nakama server
+-- @module nakama_helper
+-- @local
+local app = require("eva.app")
+local log = require("eva.log")
+local luax = require("eva.luax")
+local const = require("eva.const")
+local defold = require("nakama.engine.defold")
+local nakama = require("nakama.nakama")
+local nakama_session = require("nakama.session")
+local nakama_log = require("nakama.util.log")
+
+local device = require("eva.modules.device")
+
+local logger = log.get_logger("nakama")
+
+local M = {}
+
+
+local function on_disconnect(message)
+	logger:debug("Server disconnected")
+	app.server_data.is_connected = false
+	app.server_events.event_disconnect:trigger(message)
+	app.server_events.connect_status:trigger(false)
+end
+
+
+local function on_notification(message)
+	local data = message.notifications.notifications or {}
+	app.server_events.event_notification:trigger(data)
+end
+
+
+local function on_matchdata(message)
+	app.server_events.event_matchdata:trigger(message)
+end
+
+
+local function on_matchpresence(message)
+	app.server_events.event_matchpresence:trigger(message)
+end
+
+
+local function on_matchmakermatched(message)
+	app.server_events.event_matchmakermatched:trigger(message)
+end
+
+
+local function on_statuspresence(message)
+	app.server_events.event_statuspresence:trigger(message)
+end
+
+
+local function bind_callbacks(client, socket)
+	nakama.on_disconnect(socket, on_disconnect)
+	nakama.on_notification(socket, on_notification)
+	nakama.on_matchdata(socket, on_matchdata)
+	nakama.on_matchpresence(socket, on_matchpresence)
+	nakama.on_matchmakermatched(socket, on_matchmakermatched)
+	nakama.on_statuspresence(socket, on_statuspresence)
+end
+
+
+local function set_session_token(client, session)
+	if session and session.token then
+		app[const.EVA.SERVER].token = session.token
+		app.server_data.nakama_session = session
+		nakama.set_bearer_token(client, session.token)
+	else
+		app[const.EVA.SERVER].token = ""
+		app.server_data.nakama_session = nil
+	end
+end
+
+
+local function is_token_empty(token)
+	return not token or token == ""
+end
+
+
+local function is_session_expire_soon(session)
+	local time_to_expire = (session.expires - os.time())
+	return time_to_expire < app.settings.server.check_connection_timer * 5
+end
+
+
+local function auth(client)
+	local uid = device.get_device_id()
+	local body = nakama.create_api_account_device(uid)
+	logger:debug("Auth started", { id = uid })
+
+	local session = nakama.authenticate_device(client, body, true, uid)
+	if not session.token then
+		logger:info("Auth failed, no session")
+		return nil
+	end
+
+	set_session_token(client, session)
+
+	logger:debug("Authenticated", { token = session.token, user_id = session.user_id })
+	return session.token
+end
+
+
+local function check_new_user(client, token)
+	if is_token_empty(token) then
+		logger:debug("Auth for new user")
+		token = auth(client)
+	end
+
+	return token
+end
+
+
+local function check_session(client, token)
+	local session = nakama_session.create({ token = token })
+	if is_session_expire_soon(session) then
+		logger:info("Session has expired, call reauth")
+		token = auth(client)
+	else
+		logger:debug("Session token is valid")
+		set_session_token(client, session)
+	end
+	return token
+end
+
+
+local function socket_connect(client, socket, callback)
+	local socket_connected, socket_error = nakama.socket_connect(socket)
+	app.server_data.is_connecting = false
+
+	if socket_connected then
+		logger:info("Socket connected")
+		app.server_data.is_connected = true
+		app.server_events.connect_status:trigger(true)
+		if callback then
+			callback()
+		end
+	else
+		logger:error("Socket error", { error = socket_error })
+		set_session_token(nil)
+
+		if luax.string.ends(socket_error, "401") then
+			logger:info("Token expired, reset")
+			M.connect(client, socket, nil, callback)
+		end
+	end
+end
+
+
+--- Return nakama client config
+-- @function nakama_helper.create_nakama_client
+-- @treturn string host
+-- @treturn number port
+-- @treturn boolean is_ssl
+-- @treturn table The nakama client config
+function M.create_nakama_client(host, port, is_ssl)
+	if app.settings.server.debug_log then
+		nakama_log.custom(function(text, a, b)
+			logger:debug(text, { a = a, b = b})
+		end)
+	end
+
+	local config = {
+		host = host,
+		port = port,
+		use_ssl = is_ssl,
+		username = app.settings.server.server_key,
+		password = "",
+		engine = defold,
+		timeout = 10, -- connection timeout in seconds
+	}
+	return nakama.create_client(config)
+end
+
+
+--- Return nakama socket client
+-- @function nakama_helper.create_nakama_socket
+-- @tparam table client The nakama client config
+-- @treturn table The nakama socket config
+function M.create_nakama_socket(client)
+	local socket = nakama.create_socket(client)
+	return socket
+end
+
+
+--- Start connect to the nakama server
+-- @function nakama_helper.connect
+-- @tparam table client The nakama client config
+-- @tparam table socket The nakama socket config
+-- @tparam string token The nakama client token
+-- @tparam function callback The callback after the login
+function M.connect(client, socket, token, callback)
+	if app.server_data.is_connected then
+		logger:error("Call server connect while already connected")
+		return
+	end
+
+	app.server_data.is_connecting = true
+	nakama.sync(function()
+		token = check_new_user(client, token)
+
+		if is_token_empty(token) then
+			logger:warn("Can't auth on server")
+			app.server_data.is_connecting = false
+			return nil
+		end
+
+		token = check_session(client, token)
+
+		if is_token_empty(token) then
+			logger:warn("Can't auth on server, drop the connection flow")
+			app.server_data.is_connecting = false
+			return nil
+		end
+
+		socket_connect(client, socket, callback)
+	end)
+end
+
+
+--- Check and update if required the nakama session
+-- @function nakama_helper.refresh_session
+function M.refresh_session()
+	local session = app.server_data.nakama_session
+	if not session or is_session_expire_soon(session) then
+		logger:debug("Session token will expire soon, refresh")
+		nakama.sync(function()
+			auth(app.server_data.nakama_config)
+		end)
+	end
+end
+
+
+function M.before_eva_init(client, socket)
+	bind_callbacks(client, socket)
+
+	app.server_data.is_connected = false
+	app.server_data.is_connecting = false
+end
+
+
+return M
